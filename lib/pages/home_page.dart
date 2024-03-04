3226f277a7b56e6837283755b323dc8e780b78ad
Jailton Monteiro <jailtonmonteiro147@gmail.com>
2024-03-04 12:23:20 -0300

    Inicio de projeto
    Iniciando o projeto de calculadora de IMC

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Calculadora de IMC',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.refresh,
+              color: Colors.white,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
